eefdd03a7f02b3da6205e7e3671b9fb2bd9ef90c
Stefan Aumüller <der.aumueller@gmail.com>
2019-09-10 09:05:42 +0200

    Create blinklicht1.cpp

---
+++ blinklicht1.cpp
@@ -0,0 +1,9 @@
+int led1 = 12; //Led als led1 an Pin 12
+void setup() {
+  pinMode(led1,OUTPUT); //Definiere Pin 12 (LED 1) als output
+    }
+void loop() {
+  digitalWrite(led1,HIGH); //setze LED 1 auf high
+  delay(1000); //warte 1000ms (1 sek)
+  digitalWrite(led1,LOW); //setze LED 1 auf low
+   delay(1000); //warte 1000ms (1sek)
